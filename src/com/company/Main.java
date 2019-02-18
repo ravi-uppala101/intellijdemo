94ff4e5ce12b674db5e0197a54e0d76a0455ef10
ruppala <ruppala@collegeboard.org>
2019-02-18 09:40:09 -0500

    Add Hello world message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("I am testing version control");
 	// write your code here
     }
 }
